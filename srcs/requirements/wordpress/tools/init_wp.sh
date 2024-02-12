b1cc0bd76783d9385238675a99b78af6aa775e37
trimia <marianimatteomaria@gmail.com>
2024-02-12 13:59:55 +0100

    site import to try

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -9,24 +9,23 @@
 #     sleep 1
 # done
 
-cd /var/www/html/
-wp-cli config create --dbname=${DBWP_NAME} --dbuser=${DBWP_USER} --dbpass=${DBWP_PWD} --dbhost=${DATABASE_HOST} --dbcharset="utf8" --config-file="/var/www/html/wp-config.php" --allow-root && echo "wpdb created"
-wp-cli core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-wp-cli user create ${WP_USR} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow-root
-
-# rc-service php-fpm82 stop
-/usr/sbin/php-fpm82 -FR
-
-
-# rc-service /usr/sbin/php-fpm82 start
 # cd /var/www/html/
 # wp-cli config create --dbname=${DBWP_NAME} --dbuser=${DBWP_USER} --dbpass=${DBWP_PWD} --dbhost=${DATABASE_HOST} --dbcharset="utf8" --config-file="/var/www/html/wp-config.php" --allow-root && echo "wpdb created"
 # wp-cli core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-# # wp-cli config set WP_HOME "http://localhost:9000"
-# # wp-cli config set WP_SITEURL "http://localhost:9000"
 # wp-cli user create ${WP_USR} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow-root
-# # wp-cli theme install astra --activate --allow-root
-# rc-service /usr/sbin/php-fpm82 stop
-# /usr/sbin/php-fpm82 -F
+
+# # rc-service php-fpm82 stop
+# /usr/sbin/php-fpm82 -FR
+
+
+cd /var/www/html/
+wp-cli config create --dbname=${DBWP_NAME} --dbuser=${DBWP_USER} --dbpass=${DBWP_PWD} --dbhost=${DATABASE_HOST} --dbcharset="utf8" --config-file="/var/www/html/wp-config.php" --allow-root && echo "wpdb created"
+wp-cli core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+# wp-cli config set WP_HOME "http://localhost:9000"
+# wp-cli config set WP_SITEURL "http://localhost:9000"
+wp-cli user create ${WP_USR} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow-root
+wp-cli theme install astra --activate --allow-root
+wp-cli import inception.WordPress.2024-02-10.xml --authors=create
+/usr/sbin/php-fpm82 -F
 
 
